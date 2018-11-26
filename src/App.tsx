f791cd3cf3f8607b9b5354039e9c89b3b55bb6a9
bibinraj <bibin301@gmail.com>
2018-11-26 12:47:24 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import './App.css';
+
+
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="">
+      <div className="container">
+      test
+        </div>
+      
+       
+      </div>
+    );
+  }
+}
+
+export default App;
